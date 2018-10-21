86c8b198abff2c35fbd20880bbc35c2ea52d3efc
Reviako Vladislv <vreviaka@gmail.com>
2018-10-21 15:53:07 +0300

    add files with langage

---
+++ assets/javascript/main.js
@@ -0,0 +1,22 @@
+if (sessionStorage.getItem('language')) {
+    var language = sessionStorage.getItem('language');
+    changeLanguage(language);
+} else {
+    changeLanguage('pol');
+}
+
+
+function attr() {
+    lang = document.getElementsByClassName("sell__location__flag")[0].getAttribute("data-value");
+    if (lang === "en") {
+        changeLanguage('pol');
+    } else {
+        changeLanguage('en');
+    }
+}
+
+function changeLanguage(language) {
+    document.getElementsByClassName("sell__location__flag")[0].setAttribute("data-value", language);
+    document.getElementsByClassName("sell__location__flag")[0].setAttribute("src", "assets/images/icons/" + language + ".png");
+    sessionStorage.setItem('language', language);
+}
